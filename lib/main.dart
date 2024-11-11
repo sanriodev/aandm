50d07d05ccd05a38e6d34e7239a3f83862639bdd
Matteo Juen <matteo.juen@ematric.com>
2024-11-11 20:51:42 +0100

    feat: some stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:aandm/constants/theme.dart';
-import 'package:aandm/models/countdown_values.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,20 +28,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final CountDownValues countDownValues = CountDownValues.getValues();
-  int days = 0;
-  int hours = 0;
-  int minutes = 0;
   int seconds = 0;
+  int buttonPressCounter = 0;
   @override
   void initState() {
-    countDownValues.getCountDownStream().listen((timeData) => setState(() {
-          days = timeData.days;
-          hours = timeData.hours;
-          minutes = timeData.minutes;
-          seconds = timeData.seconds;
-        }));
     super.initState();
+    seconds = getSecondsUntilNextFriday();
+  }
+
+  incrementCounter() {
+    setState(() {
+      buttonPressCounter++;
+    });
+  }
+
+  int getSecondsUntilNextFriday() {
+    //this should get the number of seconds from now until the next friday at 6pm
+    int secondsDiff = 0;
+    DateTime now = DateTime.now();
+    if (now.weekday <= 5) {
+      var dayDiff = 5 - now.weekday;
+      secondsDiff = DateTime(now.year, now.month, now.weekday + dayDiff, 18, 00)
+          .difference(now)
+          .inSeconds;
+    } else if (now.weekday == 5) {
+      secondsDiff = DateTime(now.year, now.month, now.weekday, 18, 00)
+          .difference(now)
+          .inSeconds;
+    } else if (now.weekday > 5) {
+      var dayDiff = (now.weekday + 5) % 7;
+      secondsDiff = DateTime(now.year, now.month, now.day + dayDiff, 18, 00)
+          .difference(now)
+          .inSeconds;
+    }
+    return secondsDiff;
   }
 
   @override
@@ -52,10 +70,11 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
-          leading: ElevatedButton(onPressed: () {
-            Fluttertoast.showToast(msg: "I love my girl so much");
-          },
-          child: const Text("press me")),
+          leading: ElevatedButton(
+              onPressed: () {
+                incrementCounter();
+              },
+              child: const Icon(Icons.heart_broken_outlined)),
         ),
         body: Center(
           child: Column(
@@ -71,21 +90,27 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('$days days.'),
+                child: Text('${seconds / 60 / 60 / 24} days.'),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('$hours hours.'),
+                child: Text('${seconds / 60 / 60} hours.'),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('$minutes minutes.'),
+                child: Text('${seconds / 60} minutes.'),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text('$seconds seconds.'),
               ),
-              const Padding(padding: EdgeInsets.all(8.0), child: Text("I love my girlfriend so much omg!!!", style: TextStyle(fontWeight: FontWeight.bold,)),)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("I love my girlfriend $buttonPressCounter times",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    )),
+              )
             ],
           ),
         ));
